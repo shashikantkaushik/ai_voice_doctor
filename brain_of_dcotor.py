84ae7165286655ed6b4daec8c7aa325c402825cc
Shashi Kant <shashikantkaushik4@gmail.com>
2025-04-03 02:18:23 +0530

    voice of doctor

--- brain_of_dcotor.py
+++ brain_of_dcotor.py
@@ -1,45 +1,50 @@
+# if you dont use pipenv uncomment the following:
+# from dotenv import load_dotenv
+# load_dotenv()
+
+#Step1: Setup GROQ API key
 import os
-
 from dotenv import load_dotenv
-
 load_dotenv()
-
 GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
-print(GROQ_API_KEY)
+
+#Step2: Convert image to required format
+import base64
 
 
-import  base64
+#image_path="acne.jpg"
 
-image_path= "acne.jpeg"
-image_file=open(image_path,"rb")
-encoded_image=base64.b64encode(image_file.read()).decode('utf-8')
+def encode_image(image_path):   
+    image_file=open(image_path, "rb")
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
+#Step3: Setup Multimodal LLM 
 from groq import Groq
 
-client=Groq(api_key=GROQ_API_KEY)
-query="is there something wrong with my face"
-model= "llama-3.2-90b-vision-preview"
-messages = [
-    {
-        "role": "user",
-        "content": [
-            {
-                "type": "text",
-                "text": query
-            },
-            {
-                "type": "image_url",
-                "image_url": {
-                    "url": f"data:image/jpeg;base64,{encoded_image}",
+query="Is there something wrong with my face?"
+model="llama-3.2-90b-vision-preview"
+
+def analyze_image_with_query(query, model, encoded_image):
+    client=Groq()  
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text", 
+                    "text": query
                 },
-            },
-        ],
-    }]
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{encoded_image}",
+                    },
+                },
+            ],
+        }]
+    chat_completion=client.chat.completions.create(
+        messages=messages,
+        model=model
+    )
 
-chat_completion=client.chat.completions.create(
-    messages=messages,
-    model=model
-)
-
-# print(chat_completion)
-print(chat_completion.choices[0].message.content)
+    return chat_completion.choices[0].message.content
